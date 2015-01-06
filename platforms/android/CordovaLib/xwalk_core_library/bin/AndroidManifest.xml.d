60e14d3375ee8ea38b0426f2db6bce834abf65f4
Ben Tillman <ben.tillman@gmail.com>
2015-01-06 15:35:29 +1300

    remove crosswalk dependency resolves #4

--- platforms/android/CordovaLib/xwalk_core_library/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/warp/code/cordova/doubler/platforms/android/CordovaLib/xwalk_core_library/bin/AndroidManifest.xml : \
-/Users/warp/code/cordova/doubler/platforms/android/CordovaLib/xwalk_core_library/AndroidManifest.xml \
