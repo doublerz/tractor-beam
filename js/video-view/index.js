786d95c264faecb9b1d85211f13e4cae5393bfd0
Ben Tillman <ben.tillman@gmail.com>
2015-10-02 15:18:23 +1300

    applies standard format

--- js/video-view/index.js
+++ js/video-view/index.js
@@ -1,98 +1,89 @@
-var debug = require('debug')('app:video-view');
 var Peer = require('peerjs')
+var fs = require('fs')
 
-var fs = require("fs");
-var tmpl = fs.readFileSync(__dirname + "/index.html", "utf-8");
+var arduino = require('../lib/arduino')()
+var tmpl = fs.readFileSync(__dirname + '/index.html', 'utf-8')
 
-function VideoView() {
+function VideoView () {
   if (!(this instanceof VideoView)) {
-    return new VideoView();
+    return new VideoView()
   }
 
-  var self = this;
-  self.el = document.createElement('div');
-  self.el.innerHTML = tmpl;
-};
+  var self = this
+  self.el = document.createElement('div')
+  self.el.innerHTML = tmpl
+}
 
-VideoView.prototype.connect = function(){
+VideoView.prototype.connect = function () {
+  window.peer = this._peer = new Peer('robot1', {key: process.env.PEERJS_KEY, debug: 3})
 
-  window.peer = this._peer = new Peer('robot1', {key: process.env.PEERJS_KEY, debug: 3}),
-      arduino = require('../lib/arduino')(),
-      watchdog = null,
-      watchdogTime = 500; // ms
+  this._peer.on('open', function () {
+    document.getElementById('my-id').innerHTML = this._peer.id
+  })
 
-  peer.on('open', function() {
-    document.getElementById('my-id').innerHTML = peer.id;
-  });
-
-  peer.on('call', function(call) {
-    call.answer(window.localStream);
+  this._peer.on('call', function (call) {
+    call.answer(window.localStream)
 
     // Hang up on an existing call if present
     if (window.existingCall) {
-      window.existingCall.close();
+      window.existingCall.close()
     }
 
     // Wait for stream on the call, then set peer video display
-    call.on('stream', function(stream) {
+    call.on('stream', function (stream) {
       document.getElementById('their-video')
-        .setAttribute('src', URL.createObjectURL(stream));
-    });
-
-    window.existingCall = call;
-  });
-
-  peer.on('connection', function(connection) {
-    connection.on('data', function(data) {
-      arduino.setSpeeds(data[0], data[1]);
+        .setAttribute('src', window.URL.createObjectURL(stream))
     })
-  });
 
-  peer.on('error', function(err){
+    window.existingCall = call
+  })
+
+  this._peer.on('connection', function (connection) {
+    connection.on('data', function (data) {
+      arduino.setSpeeds(data[0], data[1])
+    })
+  })
+
+  this._peer.on('error', function () {
     this.connect()
-  }.bind(this));
+  }.bind(this))
+}
 
-};
+VideoView.prototype.disconnect = function () {
+  if (this._peer) this._peer.disconnect()
+}
 
-VideoView.prototype.disconnect = function() {
-  if (this._peer) {
-    this._peer.disconnect();
-  }
-};
+VideoView.prototype.reconnect = function () {
+  if (this._peer) this._peer.reconnect()
+}
 
-VideoView.prototype.reconnect = function() {
-  if (this._peer) {
-    this._peer.reconnect();
-  }
-};
-
-VideoView.prototype.ready = function() {
+VideoView.prototype.ready = function () {
   navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia;
+    navigator.mozGetUserMedia
 
-  this.connect();
+  this.connect()
 
-  setTimeout(function() {
+  setTimeout(function () {
     document.getElementById('forward').addEventListener('click', function () {
-      console.log('forward');
-      arduino.setSpeeds(255, 255);
-    });
-  }, 1000);
+      console.log('forward')
+      arduino.setSpeeds(255, 255)
+    })
+  }, 1000)
 
-  navigator.getUserMedia({audio: true, video: true}, function(stream) {
-    window.localStream = stream;
-  }, function(err) {
-    console.error(JSON.stringify(err));
-  });
-};
+  navigator.getUserMedia({audio: true, video: true}, function (stream) {
+    window.localStream = stream
+  }, function (err) {
+    console.error(JSON.stringify(err))
+  })
+}
 
-VideoView.prototype.pause = function() {
-  this.disconnect();
-};
+VideoView.prototype.pause = function () {
+  this.disconnect()
+}
 
-VideoView.prototype.resume = function() {
-  this.reconnect();
-};
+VideoView.prototype.resume = function () {
+  this.reconnect()
+}
 
-module.exports = VideoView;
+module.exports = VideoView
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-var webpack = require('webpack');
 require('dotenv').load()
 
 module.exports = {
   context: __dirname,
-  entry: "./js/index.js",
+  entry: './js/index.js',
   output: {
-    path: __dirname + "/www",
-    filename: "index.js"
+    path: __dirname + '/www',
+    filename: 'index.js'
   },
-  devtool: "eval",
+  devtool: 'eval',
   debug: true,
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: "transform?brfs"
+        loader: 'transform?brfs'
       },
       {
         test: /\.js$/,
-        loader: "transform?envify"
+        loader: 'transform?envify'
       }
     ],
     noParse: [
       /fastclick/
     ]
   }
-};
+}
