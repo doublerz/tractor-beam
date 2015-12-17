4dff6e87412dd30eede61cfdf14c8b2b85d46336
Ben Tillman <ben.tillman@gmail.com>
2015-12-17 14:54:52 +1300

    updates cordova firmata version

--- plugins/com.github.warp.cordova-firmata/src/android/Firmata.java
+++ plugins/com.github.warp.cordova-firmata/src/android/Firmata.java
@@ -3,6 +3,8 @@ package com.github.warp.cordova.firmata;
 import java.io.*;
 import java.lang.*;
 
+import android.util.*;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -11,15 +13,24 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import org.shokai.firmata.ArduinoFirmata;
+import org.shokai.firmata.ArduinoFirmataEventHandler;
 
 public class Firmata extends CordovaPlugin {
-
+    private String TAG = "CordovaFirmata";
     private static ArduinoFirmata arduino;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (arduino == null) {
             arduino = new ArduinoFirmata(this.cordova.getActivity());
+            arduino.setEventHandler(new ArduinoFirmataEventHandler() {
+                public void onError(String errorMessage) {
+                    Log.e(TAG, errorMessage);
+                }
+                public void onClose() {
+                    Log.v(TAG, "connection closed");
+                }
+            });
         }
 
         if (action.equals("getBoardVersion")) {
